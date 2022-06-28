2e5a04b1dcaa26f4c8e0e1c56e6e28e2bf088022
hakanecavus <hakanemrecavus@gmail.com>
2022-06-28 19:21:45 +0300

    test tryout

--- app.test.js
+++ app.test.js
@@ -1,1 +1,12 @@
-const category = require('./controller/categories')
+const request = require('supertest');
+const app = require('./app');
+const categories = require('./routes/categories');
+
+describe('categories', () =>
+{
+    it('GET /categories', () =>
+    {
+        return request(categories).get('/mens').expect('Content-Type', /json/).expect(200);
+    })
+
+});
--- controllers/categories.js
+++ controllers/categories.js
@@ -9,37 +9,43 @@ const options = {
   method: 'GET',
 };
 let json;
-let data="";
+let data = "";
 var category_list = [];
-exports.getSubcategory = (req,res) =>{
-    const catId = req.params.categoryId;
-    options.path = '/api/categories/parent/'+catId+'?secretKey=$2a$08$OLe5hqWHd9T.rBHGngjc2u/I9uTC93NwOE2VI2vQ0Bmznk3tsbrHe'
+exports.getSubcategory = (req, res) =>
+{
+  const catId = req.params.categoryId;
+  options.path = '/api/categories/parent/' + catId + '?secretKey=$2a$08$OLe5hqWHd9T.rBHGngjc2u/I9uTC93NwOE2VI2vQ0Bmznk3tsbrHe'
 
-    https.get(options,resp =>{
-        resp.on("data", (chunk) => {
-          data += chunk;
-        });
-    
-        resp.on("end", () =>{
-          json = JSON.parse(data);
-          for (var i = 0; i < json.length; i++) {
-            let category = new categories(json[i].image, json[i]._id, json[i].id, json[i].page_description, json[i].page_title, json[i].parent_category_id,json[i].c_showInMenu, json[i].__v);
-            
-            if(category.image !='categories/category_404.png'){
-              category_list.push(category)
-            }
-          }
-          console.log(category_list)
-          res.render('shop/categories', {
-            category_list: category_list,
-            breadcrumbs: req.breadcrumbs,
-            pageTitle: 'All Categories',
-            path: '/categories/'+catId
-          });
-    
-          data ="";
-          category_list = [];
-        });
+  https.get(options, resp =>
+  {
+    resp.on("data", (chunk) =>
+    {
+      data += chunk;
     });
-    
+
+    resp.on("end", () =>
+    {
+      json = JSON.parse(data);
+      for (var i = 0; i < json.length; i++)
+      {
+        let category = new categories(json[i].image, json[i]._id, json[i].id, json[i].page_description, json[i].page_title, json[i].parent_category_id, json[i].c_showInMenu, json[i].__v);
+
+        if (category.image != 'categories/category_404.png')
+        {
+          category_list.push(category)
+        }
+      }
+      console.log(category_list)
+      res.render('shop/categories', {
+        category_list: category_list,
+        breadcrumbs: req.breadcrumbs,
+        pageTitle: 'All Categories',
+        path: '/categories/' + catId
+      });
+
+      data = "";
+      category_list = [];
+    });
+  });
+
 }
